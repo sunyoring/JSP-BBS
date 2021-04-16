c4ca8a8daf405a238262148380e4854578fb621d
Sunyoring <sunhyo67@gmail.com>
2021-04-16 13:39:09 +0900

    joinAction

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -43,4 +43,21 @@ public class UserDAO { //DAO : 데이터베이스 접근 객체, 데이터베이스에서 정보를 �
 		}
 		return -2; //데이터베이스 오류를 의미
 	}
+	public int join(User user) {
+		String SQL = "INSERT INTO USER VALUES (? , ? , ? , ? , ?)";
+		try {
+			pstmt = conn.prepareCall(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserEmail());
+			return pstmt.executeUpdate();
+		}catch (Exception e){
+			e.printStackTrace();
+			
+		}
+		return -1; //데이터베이스 오류
+		
+	}
 }
